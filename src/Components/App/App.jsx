2d1dab9a6e897852cb0b1b5226a7d84e3a972c77
kantapat-guy <107095121+kantapat-guy@users.noreply.github.com>
2022-07-27 02:57:23 +0700

    Finished weeklystat and tips

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from '../../assets/react.svg'
 import './App.css'
 import ActivityList from '../Page1/Activity-list/ActivityList'
+import Weeklystat from '../Page1/Sidebar/Weeklystat'
+import Tips from '../Page1/Sidebar/Tips'
 
 function App() {
   return (
     <div className='App'>
-        <ActivityList />
+        {/* <ActivityList /> */}
+        {/* <Weeklystat /> */}
+        {/* <Tips /> */}
     </div>
   )
 }
