6d89ad2b4a57be9449a698abaee12363ce0e697b
Thanawit Narumonnantakhan <mingthanawit08@gmail.com>
2022-07-27 16:29:09 +0700

    remove

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
+
 import './App.css'
 import ActivityList from '../Page1/Activity-list/ActivityList'
-import Weeklystat from '../Page1/Sidebar/Weeklystat'
-import Tips from '../Page1/Sidebar/Tips'
+
 
 function App() {
   return (
     <div className='App'>
-        {/* <ActivityList /> */}
-        {/* <Weeklystat /> */}
-        {/* <Tips /> */}
+
     </div>
   )
 }
