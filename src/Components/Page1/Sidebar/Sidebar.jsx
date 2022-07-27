21efa0fcba4965924ddbdb30c1464564f1ee64ae
Thanawit Narumonnantakhan <mingthanawit08@gmail.com>
2022-07-27 15:24:50 +0700

    finished add profile and summary button

--- src/Components/Page1/Sidebar/Sidebar.jsx
+++ src/Components/Page1/Sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
 import "./Sidebar.css";
 import Profile from './Profile'
+import Summary from "./Summary";
 
  const Sidebar = () => {
   return (
     <div className="sidebar">
       <Profile />
+      <Summary />
     </div>
   );
 };
