ab47777944dacf4f291bfd43aa1fa0609127b9c4
Thanawit Narumonnantakhan <mingthanawit08@gmail.com>
2022-07-28 01:31:07 +0700

    combine all component in page1

---
+++ src/Components/Page1/Activity-list/AddActivity.jsx
@@ -0,0 +1,14 @@
+import "./AddActivity.css";
+
+
+const AddActivity = () => {
+  return (
+    <div className="AddAct">
+      <button>
+        <h1>Add Activity</h1>
+      </button>
+    </div>
+  );
+};
+
+export default AddActivity;
