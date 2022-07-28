c7d1717e74b1212d93e4c82293944df9fc04bd9d
Ing-ing <boonyawee.ing@gmail.com>
2022-07-28 16:06:58 +0700

    Done with jsx but stuck with css

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from '../../assets/react.svg'
 import Navbar from '../Page1/Navbar/Navbar'
-import Dropdown from '.Dropdown-button/Dropdown-button'
+import Dropdown from '../Dropdown-button/Dropdown-button'
 import './App.css'
 
 function App() {
--- src/Components/Dropdown-button/Dropdown-button.jsx
+++ src/Components/Dropdown-button/Dropdown-button.jsx
@@ -1,12 +1,17 @@
 import "./Dropdown-button.css";
 
+import calendarIcon from  '../Dropdown-button/calendarIcon.svg';
+
 const Dropdown = () => {
     return (
         <div className="Dropdown-container">
-            <select>
-                <option value="monthly">Monthly</option>
-                <option value="weekly">Weekly</option>
-            </select>
+            <button type="button"> 
+            <img src={calendarIcon} />
+                <select>
+                    <option value="weekly">Weekly</option>
+                    <option value="monthly">Monthly</option>
+                </select>
+            </button>
         </div>
     );
 
