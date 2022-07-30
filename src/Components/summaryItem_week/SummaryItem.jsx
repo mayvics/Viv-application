aa73030cd48d3e7bd3b32818fbbd27ed6c3e2ebb
Korwith Khuntikul <tumtumkiki@gmail.com>
2022-07-31 02:04:22 +0700

    Finish Components summaryItem

---
+++ src/Components/summaryItem_week/SummaryItem.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './SummaryItem.css'
+
+
+const SummaryItem =() => {
+    return (
+            <div className="summary-card">
+                <div className="container-card">
+
+                    {/*         card 1        */}
+                    <div className="card">
+                        <div className="card-content">
+                                <h3>Mon</h3>
+                                <p>Running</p>
+                                <p>Weight lifting</p>
+                        </div>
+                    </div>
+
+                    {/*         card2             */}
+                    <div className="card">
+                        <div className="card-content">
+                                <h3>Tue</h3>
+                                <p>Running</p>
+                                <p>Weight lifting</p>
+                        </div>
+                    </div>
+
+                     {/*         card 3           */}
+                   
+                    <div className="card">
+                        <div className="card-content">
+                                <h3>Wed</h3>
+                                <p>Running</p>
+                                <p>Weight lifting</p>
+                        </div>
+                    </div>
+
+                    {/*         card 4            */}
+
+                    <div className="card">
+                        <div className="card-content">
+                                <h3>Thu</h3>
+                                <p>Running</p>
+                                <p>Weight lifting</p>
+                        </div>
+                    </div>
+
+                    {/*         card 5            */}
+
+                    <div className="card">
+                        <div className="card-content">
+                                <h3>Fri</h3>
+                                <p>Running</p>
+                                <p>Weight lifting</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default SummaryItem
