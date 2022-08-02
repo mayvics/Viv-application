05d7b9d270241ce918ff499f1feeb153f7441020
Ing-ing <boonyawee.ing@gmail.com>
2022-08-02 11:53:57 +0700

    Piechart added

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from '../../assets/react.svg'
 import Navbar from '../Page1/Navbar/Navbar'
 import Dropdown from '../Dropdown-button/Dropdown-button'
+import DisplayPie from '../Piechart/Piechart'
 import './App.css'
 
 function App() {
   
   return (
     <div className="App">
-      <Dropdown />
+      <DisplayPie />
     </div>
   )
 }
