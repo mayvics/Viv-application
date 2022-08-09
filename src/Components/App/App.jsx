981b5bede308167d336ebe4d7add445a8e7e8f51
kantapat-guy <107095121+kantapat-guy@users.noreply.github.com>
2022-08-09 15:50:04 +0700

    Fix some bug

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,11 +1,11 @@
-
 import "./App.css";
 import Page1 from "../Page1/Page1";
 import EditAct from "../EditActivity/EditActivity";
 import CreateAct from "../CreateActivity/CreateActivity";
 import SummaryItem from "../summaryItem_week/summaryItem";
+import Calendar from "../Calendar/Calendar";
 import Navbar from "../Page1/Navbar/Navbar";
-import {  Route, Router, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -14,9 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Page1 />} />
         <Route path="/create" element={<CreateAct />} />
-        <Route path="/edit" element={<EditAct />} /> 
-        </Routes>
-        </div>
+        <Route path="/edit" element={<EditAct />} />
+        <Route path="/schedule" element={<Calendar />} />
+      </Routes>
+    </div>
     
   );
 
