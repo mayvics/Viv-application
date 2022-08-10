020b1b1deaf4c0fdaaa89cfa827e39b4d11dac76
Ing-ing <boonyawee.ing@gmail.com>
2022-08-10 15:37:13 +0700

    Adjusted Page1 and add responsive

--- src/Components/Page1/Page1.jsx
+++ src/Components/Page1/Page1.jsx
@@ -1,14 +1,20 @@
 import "./Page1.css";
 import ActivityList from "../Page1/Activity-list/ActivityList";
 import Sidebar from "../Page1/Sidebar/Sidebar";
+import Profile from "../Page1/Sidebar/Profile"
 import Navbar from "./Navbar/Navbar";
 
 const Page1 = () => {
   return (
     <div className="Page1">
       <div className="body">
+        <div className="Profile-top" >
+            <Profile />
+        </div>
         <Sidebar />
-        <ActivityList />
+        <div className="Activity">
+          <ActivityList />
+        </div>
       </div>
     </div>
   );
