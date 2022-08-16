3b9c74ca759f680817149f188613f6d2bc8e8553
Thanawit Narumonnantakhan <mingthanawit08@gmail.com>
2022-08-16 15:07:24 +0700

    fix bug

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ActivityList from '../Page1/Activity-list/ActivityList';
+import Navbar from '../Page1/Navbar/Navbar';
+import {  Route,  Routes } from "react-router-dom";
+import Page1 from '../Page1/Page1'
+import EditAct from "../EditActivity/EditActivity";
+import CreateAct from "../CreateActivity/CreateActivity";
 import './App.css'
+import MyCalendar from '../Calendar/Calendar';
 
 function App() {
 
   return (
-    <div className='App'>
-      <ActivityList/>
-    </div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Page1 />} />
+        <Route path="/create" element={<CreateAct />} />
+        <Route path="/edit" element={<EditAct />} /> 
+        <Route path="/schedule" element={<MyCalendar />} /> 
+        </Routes>
+        </div>
     
   );
 
--- src/Components/Page1/Activity-list/AddActivity.jsx
+++ src/Components/Page1/Activity-list/AddActivity.jsx
@@ -1,12 +1,14 @@
 import "./AddActivity.css";
-
+import { Link } from "react-router-dom";
 
 const AddActivity = () => {
   return (
     <div className="AddAct">
+      <Link to="/create">
       <button>
         <h1>Add Activity</h1>
       </button>
+      </Link>
     </div>
   );
 };
