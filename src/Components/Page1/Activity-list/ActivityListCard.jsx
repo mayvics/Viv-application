8a39344b0eef629b9353c2170c54f9e41a4b842d
kantapat-guy <107095121+kantapat-guy@users.noreply.github.com>
2022-08-16 17:28:49 +0700

    add axios to activitylist

--- src/Components/Page1/Activity-list/ActivityListCard.jsx
+++ src/Components/Page1/Activity-list/ActivityListCard.jsx
@@ -10,28 +10,30 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
+import ReactPaginate from "react-paginate";
 
 
-const ActivityListCard = () => {
+const ActivityListCard = (props) => {
+    const { data } = props;
+    const [currentItems, setCurrentItems] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 5
 
-    const [activity, setActivity] = useState([]);
+  useEffect(() => {
+    // Fetch items from another resources.
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentItems(data.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(data.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, data]);
 
-    //Fetch data from database to show at card
-    const fetchData = () => {
-      axios
-      .get(`http://localhost:8080/activities`)
-      .then((res) => {
-        setActivity(res.data)
-      })
-      .catch((err) => {
-        alert(err)
-      })
-    }
-
-    useEffect(() => {
-      fetchData()
-    }, [])
-
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.length;
+    setItemOffset(newOffset);
+  };
+  
     //For change img according to  activity
     const pic = (act) => {
         if(act.ActType==="Swimming"){
@@ -47,6 +49,7 @@ const ActivityListCard = () => {
         }
     }
 
+    
     //For delete data when click on icon and show popup
     const confirmDel = (id) => {
         //popup for confirm to delete
@@ -78,36 +81,45 @@ const ActivityListCard = () => {
         .catch(err=>console.log(err))
     }
 
-    // const { id } = useParams();
-    // console.log(id)
-
-    return(
-        <div className="container-activity">
-            <div className="container-listcard">
-            {activity.map((act,index) => (
-                <div className="listCard" key={index} >
-                    <div className="card" key={index} >
-                        <div className="container-content" key={index} >
-                            <img src= {pic(act)} alt="" />
-                                <div>
-                                    <p>Activity type : {act.ActType}</p>
-                                    <p>Duration : {act.hour} hour || {act.minute} minutes</p>
-                                    <p>Date : {new Date(act.date).toLocaleDateString()}</p>
-                                    <p>Description : {act.description}</p>
-                                </div>
-                        </div>
-                        <div className="card-btn">
-                            <Link to={`/edit/${act._id}`}><img src= { edit } alt="" /></Link>
-                            <div onClick={()=>confirmDel(act._id)}><img src= { remove } /></div>
+  return (
+    <div className="container">
+        <div className="scroll">
+        <div className="posts">
+            {currentItems.map((act,index) => (
+                    <div className="listCard" key={index} >
+                        <div className="card" key={index} >
+                            <div className="container-content" key={index} >
+                                <img src= {pic(act)} alt="" />
+                                    <div>
+                                        <p>Activity type : {act.ActType}</p>
+                                        <p>Duration : {act.hour} hour || {act.minute} minutes</p>
+                                        <p>Date : {new Date(act.date).toLocaleDateString()}</p>
+                                        <p>Description : {act.description}</p>
+                                    </div>
+                            </div>
+                            <div className="card-btn">
+                                <Link to={`/edit/${act._id}`}><img src= { edit } alt="" /></Link>
+                                <div onClick={()=>confirmDel(act._id)}><img src= { remove } /></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                ))}
-            </div>
-        </div>
-    )
+                    ))}
+        </div> 
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        containerClassName= "pagegination"
+        pageLinkClassName="page-num"
+        previousClassName="page-num"
+      />
+      </div>
+    </div>
+  );
 }
 
-
 export default ActivityListCard
-
